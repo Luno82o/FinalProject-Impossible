3c0520f63c598d645246744211ee5df1385f16fc
Luno82o <vivian.love.kitty55@gmail.com>
2022-03-26 17:43:14 +0800

    add

---
+++ ACS108117/openCVtest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 20:30:34 2022
+
+@author: USER
+"""
+import cv2
+import numpy
+
+img = cv2.imread("C:\\Users\\USER\\Downloads\\S__12329008.png")
+cv2.imshow("OpenCV test", img)
+cv2.waitKey(0)
+
