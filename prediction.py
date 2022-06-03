b95fadb4aa4b2fb34079b09134302088ea85a137
Luno82o <vivian.love.kitty55@gmail.com>
2022-06-03 13:23:39 +0800

    create
    把audioPridict從realtimeTest_warning.py拉出來

---
+++ prediction.py
@@ -0,0 +1,15 @@
+from keras.models import load_model
+import librosa
+import numpy as np
+
+
+def audioPridict(filename):
+    # 載入模型
+    model = load_model('audioWarningModel.h5')
+    
+    y1, sr1 = librosa.load(filename, duration=2.97)  
+    ps = librosa.feature.melspectrogram(y=y1, sr=sr1)
+    data = np.array([ps.reshape( (128, 128, 1) )])
+    
+    prediction = np.argmax(model.predict(data), axis=-1)
+    return(prediction)
