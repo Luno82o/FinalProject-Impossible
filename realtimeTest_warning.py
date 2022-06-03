769460ec41e87b6ea479ec3ed8f7932d95c7b3ff
Luno82o <vivian.love.kitty55@gmail.com>
2022-06-03 13:24:56 +0800

    把模型預測另外拉出來到另一個函數

--- realtimeTest_warning.py
+++ realtimeTest_warning.py
@@ -1,11 +1,8 @@
-from keras.models import load_model
-import librosa
-import librosa.display
-import numpy as np
+import argparse
 
 import PyAudioRecord
 import translate
-import argparse
+import prediction
 
 
 def parse_args():
@@ -14,31 +11,24 @@ def parse_args():
     return parser.parse_args()
 
 
-def load_data(path):
-    y1, sr1 = librosa.load(path, duration=2.97)  
-    ps = librosa.feature.melspectrogram(y=y1, sr=sr1)
-    ps = np.array([ps.reshape( (128, 128, 1) )])
-    return ps
-
 if __name__ == '__main__':
-    # 載入模型
-    model = load_model('ScreamDemo1.h5')
+    
     args = parse_args()
     
     begin = input("start?(Y/N)\n")
     
     if(begin == "Y"):
         
-        if(args.path):
-            text = translate.Voice_To_Text_wav(args.path)    #將音檔轉換成文字
-            data = load_data(args.path)
-        else:
+        if(args.path==""):
             filename = PyAudioRecord.recordAudio()
             text = translate.Voice_To_Text_wav(filename)    #將音檔轉換成文字
-            data = load_data(filename)
+            prediction = prediction.audioPridict(filename)
+        else:
+            text = translate.Voice_To_Text_wav(args.path)    #將音檔轉換成文字
+            prediction = prediction.audioPridict(args.path)
+            
         print(text)
         
-        prediction = np.argmax(model.predict(data), axis=-1)
         print(prediction)
         
         if(prediction == 0):
