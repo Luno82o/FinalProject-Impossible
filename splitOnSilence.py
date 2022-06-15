6c7801a0cb26840753d796c75f71e97ae8aae543
Luno82o <vivian.love.kitty55@gmail.com>
2022-06-15 17:16:53 +0800

    Update splitOnSilence.py

--- splitOnSilence.py
+++ splitOnSilence.py
@@ -1,70 +1,36 @@
 """
-PyAudio example: Record a few seconds of audio and save to a WAVE file.
-https://people.csail.mit.edu/hubert/pyaudio/
+https://blog.csdn.net/wangqianqianya/article/details/89605298
 """
 
-import pyaudio
-import wave
-import time
-
-import argparse
-
-def parse_args():
-    parser = argparse.ArgumentParser() 
-    parser.add_argument('--folder', '-f', default='PyAudioOutput', type=str, required=False,  help='patn for wav')
-    parser.add_argument('--lable', '-l', default='test', type=str, required=False,  help='lable for wav')
-    return parser.parse_args()
-
-def recordAudio(folder='PyAudioOutput', lable='test'):
-    
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-    RECORD_SECONDS = 3
-    
-    localtime = time.localtime()
-    result = time.strftime("%y-%m-%d_%H-%M-%S", localtime)
-    WAVE_OUTPUT_FILENAME = folder + "\\" + lable + result + ".wav"
-    
-    p = pyaudio.PyAudio()
-    
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-    
-    print("* recording")
-    
-    frames = []
-    
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-    
-    print("* done recording")
-    
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-    print("save at", WAVE_OUTPUT_FILENAME)
-    return WAVE_OUTPUT_FILENAME
-
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    value = input("record?(Y/N)\n")
-    while (value == 'Y'):
-        path = recordAudio(args.folder, args.lable)
-        print(path)
-        value = input("record?(Y/N)\n")
-    
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+ 
+sound = AudioSegment.from_mp3("movie300.wav")
+loudness = sound.dBFS
+#print(loudness)
+ 
+chunks = split_on_silence(sound,
+    # must be silent for at least half a second,沉默半秒
+    min_silence_len=430,
+ 
+    # consider it silent if quieter than -16 dBFS
+    silence_thresh=-45,
+    keep_silence=400
+ 
+)
+print('总分段：', len(chunks))
+ 
+# 放弃长度小于2秒的录音片段
+for i in list(range(len(chunks)))[::-1]:
+    if len(chunks[i]) <= 2000 or len(chunks[i]) >= 10000:
+        chunks.pop(i)
+print('取有效分段(大于2s小于10s)：', len(chunks))
+ 
+'''
+for x in range(0,int(len(sound)/1000)):
+    print(x,sound[x*1000:(x+1)*1000].max_dBFS)
+'''
+ 
+for i, chunk in enumerate(chunks):
+    chunk.export("cutFilter300/chunk{0}.wav".format(i), format="wav")
+    #print(i)
