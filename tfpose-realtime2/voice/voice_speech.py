2977f5ca4f1389d469fd047d3191bce10b88cf25
Luno82o <85742689+Luno82o@users.noreply.github.com>
2022-08-05 16:24:24 +0800

    Update voice_speech.py

--- tfpose-realtime2/voice/voice_speech.py
+++ tfpose-realtime2/voice/voice_speech.py
@@ -1,32 +1,33 @@
 import speech_recognition as sr
 
 
-#--------------------------------------
+# --------------------------------------
 # Capture Voice
 # takes command through microphone
 def recordStatement():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("listening.....")
-		r.pause_threshold = 0.5
-		audio = r.listen(source)
-	return audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 0.5
+        audio = r.listen(source)
+    return audio
+
 
 def recognizeCommand(audio):
-	r = sr.Recognizer()
-	try:
-		print("Recognizing.....")
-		query = r.recognize_google(audio, language='zh-tw')
-	except Exception as e:
-		print("say that again please.....")
-		return "None"
-	return query
+    r = sr.Recognizer()
+    try:
+        print("Recognizing.....")
+        query = r.recognize_google(audio, language='zh-tw')
+    except Exception as e:
+        print("say that again please.....")
+        return "None"
+    return query
 
-#--------------------------------------
-#speech recognition
+
+# --------------------------------------
+# speech recognition
 def recognition(filepath):
-    
     audio = recordStatementwav(filepath)
     query = recognizeCommand(audio)
-        
-    return audio,query
+
+    return audio, query
